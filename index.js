e84b7872a3cdff7434d3ed0ef1e230c60258db45
kellshen <kellyshen7@gmail.com>
2018-07-11 18:48:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 const someValue = 199
 
 function scuberGreetingForFeet(someValue) {
-  if(someValue <= 400) {
-    "This one is on me!"
+  if(someValue) > 2500 {
+    "No can do.";
+  else if(someValue <= 400) {
+    "This one is on me!";
   else if(someValue >= 400) {
-    "I will gladly take your thirty bucks."
+    "I will gladly take your thirty bucks.";
+  }
   }
   }
 }
