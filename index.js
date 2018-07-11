4106d7cb262f8f977353b7aa43847fe17cb4c09d
kellshen <kellyshen7@gmail.com>
2018-07-11 18:44:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 // Write your code in this file!
-someNumber = 199
+const someNumber = 199
 
-let someNumber
-
-function scuberGreetingForFeet(someNumber) {
-  if (someNumber <= 400) {"This one is on me!"
-  }
-}
+function 
